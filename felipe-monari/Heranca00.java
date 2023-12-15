db26bc001e62ba40f102b1b02587e89f82b24521
Monari14 <133614580+Monari14@users.noreply.github.com>
2023-12-15 00:43:49 +0000

    Segundo Commit 14/12

--- felipe-monari/Heranca00.java
+++ felipe-monari/Heranca00.java
@@ -1,32 +1,36 @@
 public class Heranca00 {
     public static void main(String[]args){
-        Cachorro c = new Cachorro("Benzetaciuuu");
-        c.toString();
-        c.fazBarulho();
+        Cachorro deyverson = new Cachorro("Deyverson", "raça77", "cor88", "onomatopeia77", 66);
+            System.out.println(deyverson);
+        
+        Cachorro johnTextor = new Cachorro("John Textor", "raça88", "cor99", "onomatopeia99", 77);
+            System.out.println(johnTextor);
+
+        Cachorro ribamar = new Cachorro("Ribamar", "raça77", "cor77", "onomatopeia88", 88);
+            System.out.println(ribamar);
+
     }
 }
     abstract class Animal{
         String nome, onomatopeia, raca, cor;
         Integer idade;
 
-    public void fazBarulho(){
-}
     public String toString(){
-        return System.out.println(
-        "Nome: "+this.nome+ "\n";
-        "Raca: "+this.raca + "\n";
-        "Cor: "+this.cor + "\n";
-        "Idade: "+this.idade + "\n";
-        "Som: "+this.onomatopeia + "\n"
-    );
+        return 
+            "\nNome: "+this.nome+ "\n"+
+            "Raca: "+this.raca + "\n"+
+            "Cor: "+this.cor + "\n"+
+            "Idade: "+this.idade + "\n"+
+            "Som: "+this.onomatopeia + "\n";
     }
 }
-class Cachorro(){
-    Cachorro(String nome){
-        this.nome = nome;
-        this.raca = raca;
-        this.cor = cor;
-        this.idade = idade;
-        this.onomatopeia = onomatopeia;
+
+    class Cachorro extends Animal{
+        Cachorro(String nome, String raca, String cor, String onomatopeia, Integer idade){
+            this.nome = nome;
+            this.raca = raca;
+            this.cor = cor;
+            this.idade = idade;
+            this.onomatopeia = onomatopeia;
     }
 }
