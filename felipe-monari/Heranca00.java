b357e3ae42d72de45470bcac51fad0c759f4706f
Monari14 <133614580+Monari14@users.noreply.github.com>
2023-12-15 18:18:41 -0300

    Format Heranca00.java

--- felipe-monari/Heranca00.java
+++ felipe-monari/Heranca00.java
@@ -1,36 +1,40 @@
+
 public class Heranca00 {
-    public static void main(String[]args){
+
+    public static void main(String[] args) {
         Cachorro deyverson = new Cachorro("Deyverson", "raça77", "cor88", "onomatopeia77", 66);
-            System.out.println(deyverson);
-        
+        System.out.println(deyverson);
+
         Cachorro johnTextor = new Cachorro("John Textor", "raça88", "cor99", "onomatopeia99", 77);
-            System.out.println(johnTextor);
+        System.out.println(johnTextor);
 
         Cachorro ribamar = new Cachorro("Ribamar", "raça77", "cor77", "onomatopeia88", 88);
-            System.out.println(ribamar);
+        System.out.println(ribamar);
 
     }
 }
-    abstract class Animal{
-        String nome, onomatopeia, raca, cor;
-        Integer idade;
 
-    public String toString(){
-        return 
-            "\nNome: "+this.nome+ "\n"+
-            "Raca: "+this.raca + "\n"+
-            "Cor: "+this.cor + "\n"+
-            "Idade: "+this.idade + "\n"+
-            "Som: "+this.onomatopeia + "\n";
+abstract class Animal {
+
+    String nome, onomatopeia, raca, cor;
+    Integer idade;
+
+    public String toString() {
+        return "\nNome: " + this.nome + "\n"
+                + "Raca: " + this.raca + "\n"
+                + "Cor: " + this.cor + "\n"
+                + "Idade: " + this.idade + "\n"
+                + "Som: " + this.onomatopeia + "\n";
     }
 }
 
-    class Cachorro extends Animal{
-        Cachorro(String nome, String raca, String cor, String onomatopeia, Integer idade){
-            this.nome = nome;
-            this.raca = raca;
-            this.cor = cor;
-            this.idade = idade;
-            this.onomatopeia = onomatopeia;
+class Cachorro extends Animal {
+
+    Cachorro(String nome, String raca, String cor, String onomatopeia, Integer idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.cor = cor;
+        this.idade = idade;
+        this.onomatopeia = onomatopeia;
     }
 }
