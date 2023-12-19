117d499d650019a56a64e9a44dcf589a16028249
BeSimonetti <151688438+BeSimonetti@users.noreply.github.com>
2023-12-19 23:34:16 +0000

    class Gato separada mod

--- bernardo/Gato.java
+++ bernardo/Gato.java
@@ -1,5 +1,5 @@
 class Gato extends Animal{
     Gato(String nome, String raca, String cor, Integer idade){
-        super(nome,"miaaaauuuuuu", cor, idade, raca);
+        super(nome, cor, raca, idade, "miaaaauuuuuu");
     }
 }
