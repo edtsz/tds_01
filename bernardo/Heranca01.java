ff88b39f452384c1b967918524f4c35fd5b1e8f5
BeSimonetti <151688438+BeSimonetti@users.noreply.github.com>
2023-12-19 23:15:44 +0000

    main Heranca01 separada

--- bernardo/Heranca01.java
+++ bernardo/Heranca01.java
@@ -1,71 +1,21 @@
 public class Heranca01 {
     public static void main(String[] args) {
         
-        Cachorro fred = new Cachorro("fred", "Vira-lata", "preto", "auau", 5);
+        Cachorro fred = new Cachorro("fred", "Vira-lata", "preto", 5);
         System.out.println(fred);
 
-        Cachorro thor = new Cachorro("Thor","Pinscher", "Marrom", "auauauauau", 15);
+        Cachorro thor = new Cachorro("Thor","Pinscher", "Marrom", 15);
         System.out.println(thor);
 
-        Cachorro romeu = new Cachorro("Romeu","Salsicha","Caramelo","auau", 9);
+        Cachorro romeu = new Cachorro("Romeu","Salsicha","Caramelo", 9);
         System.out.println(romeu);
 
-        Gato pelucio = new Gato("Pelucio","Persa","Caramelo","miaaauuuu", 3);
+        Gato pelucio = new Gato("Pelucio","Persa","Caramelo", 3);
         System.out.println(pelucio);
     
-        Cavalo datsun = new Cavalo("Datsun","Puro-Sangue-Árabe","Dourado","bruuuuu", 10);
+        Cavalo datsun = new Cavalo("Datsun","Puro-Sangue-Árabe", "Dourado", 10);
         System.out.println(datsun);
+        
     }
 
-}
-
-abstract class Animal {
-    String nome;
-    String onomatopeia; // representacao sonora emitida pelo animal
-    String cor;
-    Integer idade;
-    String raca;
-
-    public void fazBarulho() {
-        System.out.println(this.onomatopeia);
-    }
-
-    public String toString() { // representa a classe atual em forma de string
-        return  "Nome: " + this.nome + "\n"+
-                "Raça: " + this.raca + "\n"+
-                "Cor: " + this.cor + "\n"+
-                "Idade: " + this.idade + "\n"+
-                "Som: " + this.onomatopeia+"\n";
-    }
-}
-
-class Cachorro extends Animal{ 
-    
-    Cachorro(String nome, String raca, String cor, String onomatopeia, Integer idade){
-        this.nome = nome;    
-        this.raca = raca;
-        this.cor = cor;
-        this.idade = idade;
-        this.onomatopeia = onomatopeia;
-    }
-}
-
-class Gato extends Animal{
-    Gato(String nome, String raca, String cor, String onomatopeia, Integer idade){
-        this.nome = nome;    
-        this.raca = raca;
-        this.cor = cor;
-        this.idade = idade;
-        this.onomatopeia = onomatopeia;
-    }
-}
-
-class Cavalo extends Animal{ 
-    Cavalo(String nome, String raca, String cor, String onomatopeia, Integer idade){
-        this.nome = nome;    
-        this.raca = raca;
-        this.cor = cor;
-        this.idade = idade;
-        this.onomatopeia = onomatopeia;
-    }
 }
