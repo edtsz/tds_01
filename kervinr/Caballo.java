64e0ff0b9cac9a14274f262de5bd493627712473
Kervin-Rolp <112329983+Kervin-Rolp@users.noreply.github.com>
2023-12-19 21:23:04 -0300

    Primer PR (#3)
    * add files
    
    * Commit de Herancas
    
    * Colocando Herancias String toString
    
    * Tercer Commit
    
    * add de classe gato
    
    * Creacion de carpeta tds
    
    * alteraciones
    
    * Idtc
    
    * Separacion de Classes
    
    * Reset
    
    * modificacion de espacios
    
    * Modificacion Onomatopeia
    
    * Modificacion Eliminacion de la Onomatopeia
    
    * Modificacion Eliminacion

---
+++ kervinr/Caballo.java
@@ -0,0 +1,10 @@
+class Caballo extends Animal {
+    Caballo(String nome, String raca, String cor, String idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.cor = cor;
+        this.idade = idade;
+        this.onomatopeia = "BRRRRUUU";
+    }
+
+}
---
+++ kervinr/Cachorro.java
@@ -0,0 +1,9 @@
+class Cachorro extends Animal {
+    Cachorro(String nome, String raca, String cor, String idade) {
+        this.nome = nome;
+        this.raca = raca;
+        this.cor = cor;
+        this.idade = idade;
+        this.onomatopeia = "UAUAU";
+    }
+}
