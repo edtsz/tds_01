2f97b498caad0e4254b2150f87adb32a8ee17463
Guilherme-Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-02-02 00:32:29 +0000

    posicao nao terminado

---
+++ Guilherme/Posicao.java
@@ -0,0 +1,12 @@
+public class Posicao {
+
+    public static void main (String [] args) {
+
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Qual é o teu nome? ");
+        String texto3 = obj.nextLine();
+         /*não terminado*/
+    }
+
+}
+
