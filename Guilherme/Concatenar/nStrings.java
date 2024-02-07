826d7a3425d604f6013e5cb978476143c77dfe42
Guilherme-Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-02-07 00:39:43 +0000

    Não acabei

---
+++ Guilherme/Concatenar/nStrings.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class nStrings{
+
+    public static void main (String [] args ){
+       Integer letraPos = Integer.parseInt(args[0]);
+       String a = args [1];
+       System.out.println(" - " + ( a.charAt(letraPos - 1)));
+       /*não terminado */
+    }
+
+
+}
