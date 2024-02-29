fd06a9cedd200454a5262f6d3b519bd6e49ee76c
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-02-29 23:07:14 +0000

    media e soma feitas

--- Guilherme/Exercicios2/ex02.java
+++ Guilherme/Exercicios2/ex02.java
@@ -1,12 +1,18 @@
-public class ex01 {
+public class ex02 {
 
     public static void main (String [] args) {
 
+        float qtdnum = args.length;
         int total = 0;
+
         for (int i = 0 ; i < args.length ; i++) {
         total = total + Integer.parseInt(args[i]);
         }
+
+        double media = (double) total / qtdnum ;
+
         System.out.println("Total é " + total);
+        System.out.println("A média é: " + media);
         
     }
 }
