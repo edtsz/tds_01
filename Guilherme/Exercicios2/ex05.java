bc81ddd70883caccc29f694fa2929f00b415cb4f
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-03-01 22:33:04 +0000

    fiz mais um pouco

--- Guilherme/Exercicios2/ex05.java
+++ Guilherme/Exercicios2/ex05.java
@@ -3,6 +3,22 @@ public class ex05 {
 
     public static void main (String [] args) {
 
-        
+            int n = Integer.parseInt(n.getValue().toString());
+            int f = 1;
+            int c = n;
+            String s = "";
+
+            while (c >= 1) {
+
+                f *= c;
+                if( c == 1) {
+                    s += c ;
+                }
+                else if (c > 1) {
+                    s += c + " x ";
+                }
+                c--;
+            }  
+            lblFat.setText(s + " = " + Integer.toString(f));
+        }        
     }
-}
