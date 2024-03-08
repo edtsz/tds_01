baa40bd0e158bd057df6e020d64f1f6e1dccf28f
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-03-08 23:35:43 +0000

    nao sei pq fis comite

--- Guilherme/Exercicios/ex06.java
+++ Guilherme/Exercicios/ex06.java
@@ -1,1 +1,1 @@
-/*tudominúsculo*/public class ex06{public static void main(String[]args ){String min=args[0];System.out.println(min.toLowerCase());}}
+/*tudominúsculo*/public class ex06{public static void main(String[]args){String min=args[0];System.out.println(min.toLowerCase());}}
