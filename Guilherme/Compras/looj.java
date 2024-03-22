6b355a16813ccf6bf0dc6be293dc79cb70e56003
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-03-22 00:36:51 +0000

    quase acabemo

--- Guilherme/Compras/looj.java
+++ Guilherme/Compras/looj.java
@@ -9,19 +9,40 @@ public class looj {
         usuario usuario = new usuario();
         produto produto = new produto();
 
-        
+        System.out.println("-----------------------------------------------");
         System.out.println("Bom dia, vamos fazer tuas compras bagual veio: ");
+        System.out.println("-----------------------------------------------");
 
         System.out.println("Me diza teu nome: ");
-        usuario.setUsuario(scan.nextLine());
+        usuario.setNome(scan.nextLine());
 
         System.out.println("Agora me dis teu e meiol");
-        usario.setEmail(scan.nextLine());
+        usuario.setEmail(scan.next());
 
-        System.out.println("E tua senha (confia que não acontesse nada)");
+        System.out.println("E tua senha (confia que não acontesse nada):");
         usuario.setSenha(scan.next());
 
+        System.out.println("Agora vamos pra outra etapa: oq tu quer comprar?");
+        produto.setItem(scan.next());
 
+        System.out.println("E qual o presso?");
+        produto.setValor(scan.nextDouble());
+        
+        System.out.println("Me descreva tal item presente em vossa looj: ");
+        produto.setDescrissao(scan.next());
 
+        System.out.println("Ok, me diza a data.");
+        compra.setData(scan.next());
+
+        System.out.println("Agora o enderesso: ");
+        compra.setLocal(scan.next());
+
+        System.out.println("O pagamento $$$$$$$$$$$$$$$$$$$ vai ser como?");
+        compra.setPagamento(scan.next());
+
+        System.out.println("E o frete dá quanto?");
+        compra.setFrete(scan.nextDouble());
+
+        compra.imprimir();
     }
 }
--- Guilherme/Compras/produto.java
+++ Guilherme/Compras/produto.java
@@ -1,4 +1,27 @@
 public class produto {
 
-    
+    private String Item;
+    private double Valor;
+    private String Descrissao;
+
+    public void setItem(String Item) {
+        this.Item = Item;
+    }
+    public String getItem() {
+        return this.Item;
+    }
+
+    public void setValor(double Valor) {
+        this.Valor = Valor;
+    }
+    public double getValor() {
+        return this.Valor;
+    }
+
+    public void setDescrissao(String Descrissao) {
+        this.Descrissao = Descrissao;
+    }
+    public String getDescrissao() {
+        return this.Descrissao;
+    }
 }
