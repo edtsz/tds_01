17906e4dde738238059ce32ea3549449945977dd
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-04-10 00:45:14 +0000

    sim

---
+++ Guilherme/pitssaria_v0.3/pitssaria.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class pitssaria {
+
+    public static void main ( String [] args) {
+
+        System.out.println("Bem vindo à pitssaria: ");
+
+        Scanner scan = new Scanner (System.in);
+        String continuar;
+        pedidos pedidos = new pedidos();
+        String menuopc = "";
+
+        System.out.println("""
+                --------------------------------
+                Opções:
+                1.Pedidos
+                2.Imprimir Histórico
+                #.SAIR
+                --------------------------------
+                """);
+        do {
+            ItemDoPedido item = new ItemDoPedido();
+
+            System.out.println("Fala o tipo da pitssa que tu qer: ");
+            item.setTipo(scan.nextLine().trim());
+
+            System.out.println("Me diza o çabor da pitssa: ");
+            item.setSabor(scan.nextLine().trim());
+
+            pedidos.adicionarItemDoPedido(item);
+
+            System.out.println("Maiç aoguma coiza?");
+            continuar = scan.nextLine().trim();
+
+        } while (continuar.equalsIgnoreCase("sim"));
+
+        System.out.println("Nome do cliente: ");
+        pedidos.setCliente(scan.nextLine().trim());
+
+        System.out.println("Onde tu moras xoven?");
+        pedidos.setLocalEntrega(scan.nextLine().trim());
+
+        pedidos.imprimir();
+        
+    }
+}
