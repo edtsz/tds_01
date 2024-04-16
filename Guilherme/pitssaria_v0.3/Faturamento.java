1e4612e48f2657174912757dd490088ccdea9ee5
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-04-16 22:53:12 +0000

    pitssaria terminada

---
+++ Guilherme/pitssaria_v0.3/Faturamento.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class Faturamento {
+    Double totalGeral = .0;
+    Double totalMotoboy = .0;
+    Integer qtdItensDoPedido = 0;
+    private ArrayList<Pedidos> pedidos = new ArrayList<Pedidos>();
+
+    public void addPedido(Pedidos pedido) {
+        pedido.setId(this.pedidos.size() + 1);
+        this.pedidos.add(pedido);
+        this.addTotalGeral(pedido.getTotal());
+        this.addTotalMotoboy(pedido.getTaxaEntrega());
+        this.addQtdItensDoPedido(pedido.contItensDoPedido());
+    }
+
+    public void addQtdItensDoPedido(Integer qtd) {
+        this.qtdItensDoPedido += qtd;
+    }
+    public Integer getQtdItensDoPedido() {
+        return this.qtdItensDoPedido;
+    }
+    public void addTotalGeral(Double valor) {
+        this.totalGeral += valor;
+    }
+    public Double getTotalGeral() {
+        return this.totalGeral;
+    }
+    public void addTotalMotoboy(Double valor) {
+        this.totalMotoboy += valor;
+    }
+    public Double getTotalMotoboy() {
+        return this.totalMotoboy;
+    }
+
+    public Double getTotalLiquido() {
+        return this.getTotalGeral() - this.getTotalMotoboy();
+    }
+
+    public void imprimirPedido(Integer pedido_id) {
+        this.imprimirCabecalho();
+        this.pedidos.get(--pedido_id).imprimir();
+        System.out.println("--------------------------------------------");
+    }
+
+    public void imprimirRelatorio() {
+        this.imprimirCabecalho();
+        for(int i = 0; i < this.pedidos.size(); i++) {
+            if (i > 0) {
+                System.out.println();
+            }
+            this.pedidos.get(i).imprimir();
+        }
+        System.out.println("--------------------------------------------");
+        System.out.println("                   RESUMO                   ");
+        System.out.printf("%-33s :  %7d\n", "Pedidos realizados", this.pedidos.size());
+        System.out.printf("%-33s :  %7d\n", "Qtd. Pizzas", this.getQtdItensDoPedido());
+        System.out.printf("%-33s :  %7.2f\n", "Total da noite", this.getTotalGeral());
+        System.out.printf("%-33s :  %7.2f\n", "Pagar ao motoboy", this.getTotalMotoboy());
+        System.out.printf("%-33s :  %7.2f\n", "Total líquido", this.getTotalLiquido());
+        System.out.println("--------------------------------------------");
+    }
+
+    private void imprimirCabecalho() {
+        System.out.println("--------------------------------------------");
+        System.out.println("                 PITSSARIA                  ");
+        System.out.println("--------------------------------------------");
+    }
+}
--- Guilherme/pitssaria_v0.3/ItemDoPedido.java
+++ Guilherme/pitssaria_v0.3/ItemDoPedido.java
@@ -1,9 +1,6 @@
 public class ItemDoPedido {
-
-    private int n_pedido;
-    private double valor;
-    private String sabor;
     private String tipo;
+    private String sabor;
 
     public void setTipo(String tipo) {
         this.tipo = tipo;
@@ -11,7 +8,7 @@ public class ItemDoPedido {
     public String getTipo() {
         return this.tipo;
     }
-
+    
     public void setSabor(String sabor) {
         this.sabor = sabor;
     }
@@ -19,20 +16,15 @@ public class ItemDoPedido {
         return this.sabor;
     }
 
-    public void setValor(double valor) {
-        this.valor = valor;
-    }
-
-    public double getValor() {
-        if (this.getTipo().equalsIgnoreCase("tradicional")) {
+    public Double getValor() {
+        if ( this.getTipo().equalsIgnoreCase("tradicional")) {
             return 40.0;
         }
-        else if (this.getTipo().equalsIgnoreCase("especial")) {
+        else if (this.getTipo().equalsIgnoreCase("mista")) {
             return 50.0;
         }
-        else if (this.getTipo().equalsIgnoreCase("mista")) {
+        else {
             return 60.0;
         }
-        return 0.0;
     }
 }
