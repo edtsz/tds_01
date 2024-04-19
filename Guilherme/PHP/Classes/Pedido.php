9edb85b6a5145b21cdd9083a5d14614f40e595d3
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-04-19 00:46:15 +0000

    organisando

---
+++ Guilherme/PHP/Classes/Pedido.php
@@ -0,0 +1,11 @@
+<?php
+class Pedido{
+    private $ID = 0;
+    private $total = 0;
+    private $taxaEntrega = 0;
+    private $dataHoraPedido;
+    private $localEntrega;
+    private Cliente cliente;
+    private array
+}
+?>
